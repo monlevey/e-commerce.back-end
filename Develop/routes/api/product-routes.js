2a8275fecb0ffc0ad0470e8089d1fa80c59c55de
Monique Levey <monique.w.91@outlook.com>
2022-05-19 17:41:27 +0800

    product routes all working

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -26,8 +26,20 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-
+  Product.findByPk(req.params.id, {
+    include: [
+      {
+        model: Category,
+      },
+      {
+        model: Tag,
+      }
+    ]
+  }).then((product) => {
+    res.json(product);
+  })
 });
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -39,7 +51,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    name: req.body.name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -105,6 +122,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where:{
+      id: req.params.id,
+    }
+  }).then((result) => {
+    res.json({
+      data:'ok'
+    })
+  })
 });
 
 module.exports = router;

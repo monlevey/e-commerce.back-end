1ebb4909a2085fcddf0b545a17eebe113b1e81a7
Monique Levey <monique.w.91@outlook.com>
2022-05-19 14:42:24 +0800

    categories get route working- updated to res.json

--- Develop/server.js
+++ Develop/server.js
@@ -13,6 +13,6 @@ app.use(routes);
 // sync sequelize models to the database, then turn on the server
 
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on ${PORT}. Follow link http://localhost:${PORT}`));
 });
